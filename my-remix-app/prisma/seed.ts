5fe98100569d2fed368c4352c6b7e9e9e52ef544
Andrew Larder <andrew.larder@glxdigital.com>
2023-08-09 16:35:46 +0800

    initial proj

---
+++ my-remix-app/prisma/seed.ts
@@ -0,0 +1,100 @@
+import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcryptjs";
+import addDays from "date-fns/addDays";
+
+const prisma = new PrismaClient();
+
+async function seed() {
+  const email = "rachel@remix.run";
+
+  // cleanup the existing database
+  await prisma.user.delete({ where: { email } }).catch(() => {
+    // no worries if it doesn't exist yet
+  });
+
+  const getRandomNumSteps = (min: number, max: number) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+  const hashedPassword = await bcrypt.hash("racheliscool", 10);
+
+  const user = await prisma.user.create({
+    data: {
+      email,
+      password: {
+        create: {
+          hash: hashedPassword,
+        },
+      },
+    },
+  });
+
+  await prisma.stepEntry.create({
+    data: {
+      date: new Date(),
+      numSteps: getRandomNumSteps(0, 30000),
+      userId: user.id,
+    },
+  });
+
+  await prisma.stepEntry.create({
+    data: {
+      date: addDays(new Date(), 1),
+      numSteps: getRandomNumSteps(0, 30000),
+      userId: user.id,
+    },
+  });
+
+  await prisma.stepEntry.create({
+    data: {
+      date: addDays(new Date(), 2),
+      numSteps: getRandomNumSteps(0, 30000),
+      userId: user.id,
+    },
+  });
+
+  await prisma.stepEntry.create({
+    data: {
+      date: addDays(new Date(), 3),
+      numSteps: getRandomNumSteps(0, 30000),
+      userId: user.id,
+    },
+  });
+
+  await prisma.stepEntry.create({
+    data: {
+      date: addDays(new Date(), 4),
+      numSteps: getRandomNumSteps(0, 30000),
+      userId: user.id,
+    },
+  });
+
+  await prisma.stepEntry.create({
+    data: {
+      date: addDays(new Date(), 5),
+      numSteps: getRandomNumSteps(0, 30000),
+      userId: user.id,
+    },
+  });
+
+  await prisma.stepEntry.create({
+    data: {
+      date: addDays(new Date(), 6),
+      numSteps: getRandomNumSteps(0, 30000),
+      userId: user.id,
+    },
+  });
+
+  console.log(`Database has been seeded. 🌱`);
+}
+
+seed()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
